f5bb926bd81207c962af64549810125e6312fc72
selinceydeli <120125253+selinceydeli@users.noreply.github.com>
2024-11-03 14:12:22 +0100

    particle struct definition revised

--- framework/include/framework/particle.h
+++ framework/include/framework/particle.h
@@ -4,12 +4,14 @@ DISABLE_WARNINGS_PUSH()
 #include <glm/vec2.hpp>
 #include <glm/vec4.hpp>
 
-
 struct Particle {
-    glm::vec2 Position, Velocity;
-    glm::vec4 Color;
-    float     Life;
+    glm::vec2 position;
+    glm::vec2 speed;
+    glm::vec4 color;
+    float     life;
   
     Particle() 
-      : Position(0.0f), Velocity(0.0f), Color(1.0f), Life(0.0f) { }
+      : position(0.0f), speed(0.0f), color(1.0f), life(0.0f) { }
 };  
+
+
